f58cdbeb15a6aeac16e479ae3459ba8b0286721f
Martoxdlol <martoxdlol@gmail.com>
2022-11-24 15:43:24 -0300

    changes

--- start_backend.cmd
+++ start_backend.cmd
@@ -1,1 +1,1 @@
-cd server && dart run --enable-vm-service bin/server.dart 
+cd server && dart run orm generate && dart run orm db push && dart run build_runner build && dart run --enable-vm-service bin/server.dart 
