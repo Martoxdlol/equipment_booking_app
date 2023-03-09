b3ac36c5d5c8b08bc1f6457b24b0ebb6503b9c11
Martoxdlol <martoxdlol@gmail.com>
2023-03-09 15:20:09 -0300

    log files router error

--- src/server/api/routers/files/filesRouter.ts
+++ src/server/api/routers/files/filesRouter.ts
@@ -6,45 +6,55 @@ import sha256 from 'sha256'
 
 import { createTRPCRouter, namespaceAdminProcedure, namespaceProcedure, namespaceReadableProcedure } from "../../trpc";
 import { env } from "../../../../env.mjs";
+import { TRPCError } from "@trpc/server";
 
 export const filesRouter = createTRPCRouter({
     uploadImage: namespaceAdminProcedure.input(z.object({
         content: z.string(),
     })).mutation(async ({ ctx, input }) => {
-        // Content as base64 to buffer
-        const buffer = Buffer.from(input.content, 'base64')
-        // Generate sha256 hash from buffer
-        const hash = sha256(buffer)
+        try {
+            // Content as base64 to buffer
+            const buffer = Buffer.from(input.content, 'base64')
+            // Generate sha256 hash from buffer
+            const hash = sha256(buffer)
 
-        // Define possible filename
-        const filename = path.resolve(env.STORAGE_PATH, 'images', `${hash}.webp`)
-        // find possible existing file
-        const fileExists = await checkFileExists(filename)
+            // Define possible filename
+            const filename = path.resolve(env.STORAGE_PATH, 'images', `${hash}.webp`)
+            // find possible existing file
+            const fileExists = await checkFileExists(filename)
 
-        // If file exists, return url
-        if (fileExists) return {
-            url: `/api/files/images/${hash}.webp`,
-        }
+            // If file exists, return url
+            if (fileExists) return {
+                url: `/api/files/images/${hash}.webp`,
+            }
 
-        // Create sharp instance from buffer
-        let sharpInstance = sharp(buffer)
-        // Fix orientation
-        sharpInstance = sharpInstance.rotate()
-        // Resize image
-        sharpInstance = sharpInstance.resize(128, 128, { fit: 'cover' })
-        // Convert to webp buffer
-        const webpBuffer = await sharpInstance.webp().toBuffer()
-        // Generate new hash
-        const webpHash = sha256(webpBuffer)
-        // Define filename
-        const webpFilename = path.resolve(env.STORAGE_PATH, 'images', `${webpHash}.webp`)
-        // Create dir if not exists
-        await fs.mkdir(path.resolve(env.STORAGE_PATH, 'images'), { recursive: true })
-        // Write file
-        await fs.writeFile(webpFilename, webpBuffer)
+            // Create sharp instance from buffer
+            let sharpInstance = sharp(buffer)
+            // Fix orientation
+            sharpInstance = sharpInstance.rotate()
+            // Resize image
+            sharpInstance = sharpInstance.resize(128, 128, { fit: 'cover' })
+            // Convert to webp buffer
+            const webpBuffer = await sharpInstance.webp().toBuffer()
+            // Generate new hash
+            const webpHash = sha256(webpBuffer)
+            // Define filename
+            const webpFilename = path.resolve(env.STORAGE_PATH, 'images', `${webpHash}.webp`)
+            // Create dir if not exists
+            await fs.mkdir(path.resolve(env.STORAGE_PATH, 'images'), { recursive: true })
+            // Write file
+            await fs.writeFile(webpFilename, webpBuffer)
 
-        return {
-            url: `/api/files/images/${webpHash}.webp`,
+            return {
+                url: `/api/files/images/${webpHash}.webp`,
+            }
+        } catch (error) {
+            console.log("NO SE PUEDE SUBIR IMAGEN")
+            console.log(error)
+            throw new TRPCError({
+                code: 'INTERNAL_SERVER_ERROR',
+                message: 'Something went wrong while uploading the image',
+            })
         }
     }),
     getAllImages: namespaceAdminProcedure.query(async ({ ctx }) => {
